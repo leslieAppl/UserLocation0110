ffca22341f08788ee273f91187df6cfa2916a4f7
Leslie <leslie.appl@gmail.com>
2020-01-11 19:30:44 +0800

    - v0.2.0 MapKit Tutorial - Reverse Geocode - Get Address - Swift 4.2

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 User Location
 
-MapKit; CoreLocation;
-Background Process
-
-Source From: YouTube
+Source Refence: YouTube
 By: Sean Allen
 
 - v0.1.0 How to show user's location - Swift 4.2 - Xcode 10
+    - MapKit; CoreLocation;
+    - Background Process
 
+- v0.2.0 MapKit Tutorial - Reverse Geocode - Get Address - Swift 4.2
+    - GeoCoder:  A user-friendly representation of the coordinate typically consists of the street, city, state, and country information corresponding to the given location, but it may also contain a relevant point of interest, landmarks, or other identifying information. A geocoder object is a single-shot object that works with a network-based service to look up placemark information for its specified coordinate value.
+
+    - Map View - CenterCoordinate
--- UserLocation0110/MapScreen.swift
+++ UserLocation0110/MapScreen.swift
@@ -13,9 +13,12 @@ import CoreLocation
 class MapScreen: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLbl: UILabel!
     
     let locationManager = CLLocationManager()
     let REGION_IN_METERS: Double = 100
+    
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,17 +58,12 @@ class MapScreen: UIViewController {
     
     
     func checkLocationAuthorization() {
+        // In a switch statement for readablility each function involves no more 5 implementations.
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             // Do Map Stuff
-            mapView.showsUserLocation = true
-            mapView.userTrackingMode = .follow
-            centerViewOnUserLocation()
+            startTrackingUserLocation()
             
-            // starting firing off locasion manager delegate method - didUpdateLocations:
-            locationManager.startUpdatingLocation()
-            
-            break
         case .denied:
             // Show alert instructing them how to turn on permissions
             break
@@ -86,19 +84,38 @@ class MapScreen: UIViewController {
             mapView.setRegion(region, animated: true)
         }
     }
+    
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        centerViewOnUserLocation()
+        
+        // starting firing off locasion manager delegate method - didUpdateLocations:
+        locationManager.startUpdatingLocation()
+        
+        // Map View Center Location
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 
 }
 
 extension MapScreen: CLLocationManagerDelegate {
     
     // starting fired by locationManager.startUpdatingLocation()
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // check if there are any CLLocations
-        guard let location = locations.last else { return }
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: REGION_IN_METERS, longitudinalMeters: REGION_IN_METERS)
-        mapView.setRegion(region, animated: true)
-    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        // check if there are any CLLocations
+//        guard let location = locations.last else { return }
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: REGION_IN_METERS, longitudinalMeters: REGION_IN_METERS)
+//        mapView.setRegion(region, animated: true)
+//    }
     
     // delegate whenever the authorization has been changed
     // if been changed, it will auto check in the checkLocationAuthorization() function,
@@ -109,5 +126,42 @@ extension MapScreen: CLLocationManagerDelegate {
 }
 
 extension MapScreen: MKMapViewDelegate {
-    
+    // track the center of map when region did change
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        // guard make sure if 'previousLocation' has the value
+        guard let previousLocation = self.previousLocation else { return }
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        // can't call the GeoCoder all the time, otherwise it will failed
+        geoCoder.reverseGeocodeLocation(center) { [weak self](placemarks, error) in
+            guard let self = self else { return }
+            
+            if let _ = error {
+                // TODO: Show alert informing the user
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                // TODO: Show alert informing the user
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            print(streetNumber)
+            print(streetName)
+            
+            // If your app has a long running task, such as making network call, run it on a global system queue, or on another background dispatch queue. Alternatively, use asynchronous versions of the call, if available.
+            DispatchQueue.main.async {
+                self.addressLbl.text = "\(streetNumber) \(streetName)"
+            }
+        }
+        
+        
+        
+    }
 }
